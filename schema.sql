04e5d912d422ba23c69d3b00d00cb8487409a10e
Ivan Silva <ivankuartz@hotmail.com>
2022-04-25 19:30:42 -0300

    Schema and Data added

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+   id int PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   date_of_birth DATE NOT NULL,
+   escape_attempts INT  NOT NULL,
+   neutered BOOL NOT NULL,
+   weight_kg DECIMAL(7,3) NOT NULL
 );
