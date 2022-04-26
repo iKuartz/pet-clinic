5319bec8ac9cf045aa50ba8d4f1823e60a67181b
Ivan Silva <ivankuartz@hotmail.com>
2022-04-26 16:42:34 -0300

    Added queries to table

--- queries.sql
+++ queries.sql
@@ -57,3 +57,109 @@ FROM
     animals
 WHERE
     weight_kg BETWEEN 0010.400 AND 0017.300;
+
+-- Second Milestone
+
+BEGIN;
+UPDATE
+    animals
+SET
+    species = 'unspecified';
+SELECT
+    *
+FROM
+    animals;
+ROLLBACK;
+
+SELECT
+    *
+FROM
+    animals;
+
+BEGIN;
+UPDATE
+    animals
+SET
+    species = 'digimon'
+WHERE
+    name LIKE '%mon';
+UPDATE
+    animals
+SET
+    species = 'pokemon'
+WHERE
+    species IS NULL;
+COMMIT;
+
+SELECT
+    *
+FROM
+    animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+SELECT
+    *
+FROM
+    animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SV1;
+UPDATE
+    animals
+SET
+    weight_kg = animals.weight_kg * -1;
+COMMIT;
+
+SELECT
+    *
+FROM
+    animals;
+
+SELECT
+    COUNT(*)
+FROM
+    animals;
+
+SELECT
+    COUNT(*)
+FROM
+    animals
+WHERE
+    escape_attempts = 0;
+
+SELECT
+    AVG(weight_kg)
+FROM
+    animals;
+
+SELECT
+    neutered,
+    SUM(escape_attempts) AS escape_times
+FROM
+    animals
+GROUP BY
+    neutered;
+
+SELECT
+    species,
+    MIN(weight_kg),
+    MAX(weight_kg)
+FROM
+    animals
+GROUP BY
+    species;
+
+SELECT
+    species,
+    AVG(escape_attempts)
+FROM
+    animals
+WHERE
+    EXTRACT(year FROM date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY
+    species;
