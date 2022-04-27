1a597f4e37879e37130318ee1748fab47a590fed
Ivan Silva <ivankuartz@hotmail.com>
2022-04-27 10:13:35 -0300

    Corrected schema to generate id automatically as primary key

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-   id int PRIMARY KEY,
+   id SERIAL PRIMARY KEY,
    name VARCHAR(50) NOT NULL,
    date_of_birth DATE NOT NULL,
    escape_attempts INT  NOT NULL,
